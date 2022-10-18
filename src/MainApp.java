6a582369b36c5cb89d3d78f9a3eedeb600c8a8d1
sumeyyeakan <akansumeyye@gmail.com>
2022-10-18 08:22:28 +0300

    Translate

---
+++ src/MainApp.java
@@ -0,0 +1,11 @@
+/**
+ * Başlangıç sınıfıdır.
+ */
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println();
+        System.out.println("Merhaba Dünya!");
+    }
+}
